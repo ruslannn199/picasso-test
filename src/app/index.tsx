02a29e7d73a07de8609373a97f2b84ce8037d7a3
ruslannn199 <ruslannn199@gmail.com>
2023-11-22 20:46:28 +0300

    init: start project

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+const App = () => {
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import eslintPlugin from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    eslintPlugin({
+      cache: false,
+      include: ['./src/**/*.ts', './src/**/*.tsx'],
+      exclude: ['./node_modules/**'],
+    }),
+  ],
+  server: {
+    open: true,
+    port: 3000,
+  },
+})
